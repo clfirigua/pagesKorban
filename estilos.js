d7f058832b3024ac5c08c2339a7358baca07402f
clfirigua <71801587+clfirigua@users.noreply.github.com>
2022-03-07 21:20:11 -0500

    animaciones

---
+++ estilos.js
@@ -0,0 +1,29 @@
+const animate = ScrollReveal();
+
+animate.reveal('.maquinaria-form',{
+    duration: 3000,
+    origin:'left',
+    distance:'300px'
+});
+
+
+animate.reveal('.info-korban-sec',{
+    duration: 2000,
+    origin:'top',
+    distance:'300px'
+})
+
+animate.reveal('.razones-1',{
+    duration: 2000,
+    origin:'left',
+    distance:'200px'
+})
+animate.reveal('.razones-2',{
+    duration: 2000,
+    origin:'right',
+    distance:'200px'
+})
+animate.reveal('.letras',{
+    duration: 2000,
+    scale:0.85,
+})
